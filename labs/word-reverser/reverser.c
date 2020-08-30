041cffc6f2ba70ebbf49826c8c3621e2990d1c99
Nicole Carrillo C <A01551581@itesm.mx>
2020-08-29 22:10:49 -0400

    reverser nicole

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define ARRAY_LENGHT 100
 
-int main(){
-    // Place your magic here
-    return 0;
+void reverse(char arreglo[],int lenght){
+	char tmp;
+    for (int x = 0;  x < lenght/2; x++) {
+		tmp = arreglo[x];
+		arreglo[x] = arreglo[lenght - x - 1];
+		arreglo[lenght - x - 1] = tmp;
+    }
+	printf("\nReversed word: \n");
+	printf("%s", arreglo);
+	printf("\n\n");
+}
+
+int main(int argc, char **argv){
+	int c;
+	char arreglo[ARRAY_LENGHT];
+	int contador=0;
+    while ((c = getchar()) != EOF) {
+    	if (c == '\n'){  //VALIDACIÓN
+			printf("\nOriginal word: \n");
+			printf("%s", arreglo);
+       		reverse(arreglo,contador);
+			for(int x=0 ; x<contador ; x++){ //VACIO MI ARRAY PARA VOLTEAR LA SIGUIENTE PALABRA 
+				arreglo[x] = '\0';
+			}
+			contador=0; //REINICIO CONTADOR
+    	}else{
+    		arreglo[contador]=c; //GUARDO LAS LETRAS EN EL ARREGLO
+    		contador++;
+    	}
+    }
+	return 0;
 }
