f164ce4413256294fd19cd7478c3bcd0edbe8398
Nicole Carrillo C <A01551581@itesm.mx>
2020-09-01 02:15:22 -0400

    cambios

--- labs/month-day/month_day.c
+++ labs/month-day/month_day.c
@@ -1,53 +1,44 @@
 #include <stdio.h>   
 #include <stdlib.h>
-#define NUMBER_OF_STRING 13
-#define MAX_STRING_SIZE 20
-int dias[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-int diasbi[13]={0,31,29,31,30,31,30,31,31,30,31,30,31};
-int meses[13]={0,1,2,3,4,5,6,7,8,9,10,11,12};
-char mesesL[NUMBER_OF_STRING][MAX_STRING_SIZE]={"Ilegal Day","Jan","Feb","Mar","April","May","June","July","Agust","Sep","Oct","Nov","Dec"};
+char mesesL[13][20]={"Ilegal Day","Jan","Feb","Mar","April","May","June","July","Agust","Sep","Oct","Nov","Dec"};
 
 void month_day(int year, int yearday, int *pmonth, int *pday){
+	int dias[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+	int meses[13]={0,1,2,3,4,5,6,7,8,9,10,11,12};
+	pmonth=&meses[0];
+    pday=&dias[0];
     int d=*pday;
     int contador=0;
     if(year==0||yearday>366||year<=999){
         printf("ILegal Date");
         printf("\n");
-    }else if(((year%4 == 0) && (year%100 != 0)) || (year%400 == 0)){
-        while(yearday>d){
-            yearday=yearday-d;
-            pday=&diasbi[contador];
-            pmonth=&meses[contador];
-            d=*pday;
-            contador++;
-        }
-        printf("%s%s%i%s%i%s\n",mesesL[*pmonth]," ",yearday,",",year," "); 
     }else{
-        while(yearday>d){
+    	if(((year%4 == 0) && (year%100 != 0)) || (year%400 == 0)){
+        	dias[2]=29;
+         }
+        	while(yearday>d){
             yearday=yearday-d;
             pday=&dias[contador];
             pmonth=&meses[contador];
             d=*pday;
             contador++;
-        }if(*pday==0||*pmonth==0){
+        	}
+        	if(*pday==0||*pmonth==0){
             printf("ILegal Date");
             printf(" ");
-        }else{
-        printf("%s%s%i%s%i%s\n",mesesL[*pmonth]," ",yearday,",",year," "); 
+        	}else{
+        	printf("%s%s%i%s%i%s\n",mesesL[*pmonth]," ",yearday,",",year," "); 
         }  
     }
 };
-
 int main(int argc,char **argv) {
-    int *pday;
-    int *pmonth;
-    pmonth=&meses[0];
-    pday=&dias[0];
+    int pday;
+    int pmonth;
     if(argc<3){
         printf("Please enter correct form --> ");
         printf("./month_day <year> <yearday>");
         return -1;
     }
-    month_day(atoi(argv[1]),atoi(argv[2]),pmonth,pday);
+    month_day(atoi(argv[1]),atoi(argv[2]),&pmonth,&pday);
     return 0;
 }
