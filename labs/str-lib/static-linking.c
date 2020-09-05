2b304e497604c780afe0027106c77ca68a9f1586
Nicole Carrillo C <A01551581@itesm.mx>
2020-09-04 23:21:57 -0400

    str homework

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <string.h> 
 
-int main() {
+int mystrlen(char*);
+char *mystradd(char*,char*);
+int mystrfind(char*,char*);
+
+int main(int argc, char **argv) {
+    if(argc<3){
+        printf("FALTAN VALORES");
+    }else{
+        /*char *palabra=argv[1];
+        char *palabra2=argv[2];
+        mystradd(palabra,palabra2);
+        mystrfind(palabra,palabra2);*/
+        char *palabra=argv[2];
+        char *palabra2=argv[3];
+        char *lib=argv[1];
+        if(strcmp(lib,"-add")==0){
+            mystradd(palabra,palabra2);
+        }else{
+            mystrfind(palabra,palabra2);
+        }
+    }
     return 0;
 }
+
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,45 @@
-
+#include <stdio.h>   
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int contador=0;
+    while (*str != '\0') {
+        contador++,str++;
+    }
+    return contador;
 }
 
 char *mystradd(char *origin, char *addition){
+    printf("%s\t%d\n","Initial Length:",mystrlen(origin));
+    char new[mystrlen(origin)+mystrlen(addition)];
+    int i=0;
+    while (*origin!= '\0') {
+        new[i]=*origin;
+        origin++,i++;
+    }
+    while(*addition != '\0'){
+        new[i]=*addition;
+        addition++,i++;
+    }
+    new[i]='\0';
+    printf("%s\t%s\n","New String:",new); 
+    printf("%s\t%d\n","New length:",mystrlen(new));
     return 0;
 }
 
 int mystrfind(char *origin, char *substr){
+    printf("%s%s%s","['",substr,"']"); 
+    int pos=0;
+    while (*origin!= '\0') {
+        while(*origin==*substr){
+            origin++,substr++;
+            if(*substr=='\0'){
+               printf("%s%s%d%s%s\n","string was found at ","[",pos,"]"," posicion");  
+               return 0;
+            }
+        }
+        substr=&substr[0],pos++,origin++;
+    }
+    printf("%s\n","string was not found "); 
     return 0;
 }
