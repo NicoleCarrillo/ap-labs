1551c4c64573732857ee4e4ac1daebcf6a375227
Nicole Carrillo C <A01551581@itesm.mx>
2020-09-05 00:34:37 -0400

    strlab

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -6,13 +6,9 @@ char *mystradd(char*,char*);
 int mystrfind(char*,char*);
 
 int main(int argc, char **argv) {
-    if(argc<3){
-        printf("FALTAN VALORES");
+    if(argc<4){
+        printf("%s\n","FALTAN VALORES");
     }else{
-        /*char *palabra=argv[1];
-        char *palabra2=argv[2];
-        mystradd(palabra,palabra2);
-        mystrfind(palabra,palabra2);*/
         char *palabra=argv[2];
         char *palabra2=argv[3];
         char *lib=argv[1];
