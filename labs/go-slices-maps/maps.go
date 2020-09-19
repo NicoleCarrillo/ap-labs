3aa020189bd7e59c1eed289fcbad8ffe7251d40e
Nicole Carrillo C <A01551581@itesm.mx>
2020-09-18 20:45:17 -0400

    go slices maps

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"strings"
+)
+func WordCount(s string) map[string]int {
+	w:=strings.Fields(s)
+	m:=make(map[string]int)
+	for i := range w {
+        m[w[i]]++
+    }
+	return m
+}
+
 func main() {
+	WordCount("hello world")
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,11 +1,17 @@
 package main
-
-import "golang.org/x/tour/pic"
+ 
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	pic := make([][]uint8, dy)
+    for x := range pic {
+        pic[x] = make([]uint8, dx)
+        for y := range pic {
+            pic[x][y] = uint8(x^y)
+        }
+    }
+    return pic
 }
 
 func main() {
-	pic.Show(Pic)
+	Pic(100,100);
 }
