449386b6503fabbd9ac4a3794a5249c3f255cfbd
Nicole Carrillo C <A01551581@itesm.mx>
2020-11-10 20:37:24 -0500

    govst solved

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
 )
 
-func main() {
-	var commsPerSecond int
+func connection(pingIn chan int, pongOut chan int) {
+	for {
+		pongOut <- (1 + <-pingIn)
+	}
+}
 
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+func main() {
+
+	if len(os.Args) > 1 {
+		fmt.Println("WRONG USAGE : ./ping-pong ")
+
+	} else {
+		total := 0
+
+		pingIn := make(chan int)
+		pongOut := make(chan int)
+
+		go connection(pingIn, pongOut)
+		go connection(pongOut, pingIn)
+
+		for i := 0; i < 1; i++ {
+			pingIn <- 0
+			time.Sleep(time.Duration(1) * time.Second)
+			ball := <-pingIn
+			total += ball
+		}
+
+		fmt.Println("Communications Per Second : ", total)
+	}
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -1,12 +1,68 @@
+// max 2054419 pipes
 package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
+	"time"
 )
 
+var maxStages int
+
+// "https://golangcode.com/print-the-current-memory-usage/"
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Sys = %v MEMORY USE \n", bToMb(m.Sys))
+}
+func PrintMemUsageFinal() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	if m.TotalAlloc/1024/1024 > 1024 {
+		fmt.Printf("NO MORE MEMORY \n")
+		fmt.Printf("USED = %v \n", bToMb(m.Sys))
+		fmt.Println("Maximum number of pipeline stages   : ", maxStages)
+		defer os.Exit(1)
+	}
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+func test(startPipe chan int, cont int) {
+	last := startPipe
+	x := 0
+	for x < cont {
+		aux := last
+		go func() {
+			for data := range aux {
+				last <- data
+			}
+		}()
+		x++
+		maxStages++
+		PrintMemUsageFinal()
+	}
+}
+
 func main() {
-	var maxStages, transitTime int
+	startPipe := make(chan int)
+
+	var cont = 1000000
+
+	fmt.Printf("MEMORY USED BEFORE PIPE CREATION ")
+	PrintMemUsage()
+
+	startingTime := time.Now()
+	test(startPipe, cont)
+	endTime := time.Now()
+
+	transitTime := endTime.Sub(startingTime)
 
 	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
 	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+	fmt.Printf("MEMORY USED AFTER PIPE CREATION ")
+	defer PrintMemUsage()
 }
